9bd1b8f2ea46c22fd7dcceae724453bf421c370e
krishna <krishnakarki1996@gmail.com>
2024-06-14 16:49:50 +0545

    create basic class structure

---
+++ main.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QLineEdit
+
+
+class ChatBotWindow(QMainWindow):
+    def __init__(self):
+        super.__init__()
+        self.setWindowTitle("ChatBot AI")
+
+
+class ChatBot:
+    pass
