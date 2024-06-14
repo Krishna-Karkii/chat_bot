a519c61b0b11c324317eebbfce22ead2639830b6
krishna <krishnakarki1996@gmail.com>
2024-06-14 17:12:18 +0545

    Create necessary widgets

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit, QLineEdit
 
 
 class ChatBotWindow(QMainWindow):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.setWindowTitle("ChatBot AI")
+        self.setMinimumSize(700, 500)
+
+        # Text Display Widget
+        self.text_display_window = QTextEdit(self)
+        self.text_display_window.setGeometry(10, 10, 460, 320)
+        self.text_display_window.setReadOnly(True)
+
+        # input area
+        self.input_widget = QLineEdit(self)
+        self.input_widget.setGeometry(10, 340, 460, 35)
+
+        # Button Widget
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(480, 340, 60, 35)
+
+        self.show()
 
 
 class ChatBot:
     pass
+
+
+app = QApplication(sys.argv)
+chatbot_window = ChatBotWindow()
+sys.exit(app.exec())
