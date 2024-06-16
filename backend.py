8a1cb1e03f1f579b0587e3849a05bd1460f9c2c0
krishna <krishnakarki1996@gmail.com>
2024-06-16 18:16:33 +0545

    create a structure for processing

---
+++ backend.py
@@ -0,0 +1,16 @@
+import re
+import json
+import random_response
+
+
+class ChatBot:
+    def __init__(self, user_prompt):
+        self.user_prompt = user_prompt
+
+    def get_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    chat_bot = ChatBot()
+
