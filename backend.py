98547636cb8a7f4a14ff0bfe87c22b8cd45e9413
krishna <krishnakarki1996@gmail.com>
2024-06-16 23:19:05 +0545

    implement logic to find more suitable response

--- backend.py
+++ backend.py
@@ -3,14 +3,41 @@ import json
 import random_response
 
 
+with open("bot_data.json") as file:
+    contents = json.load(file)
+
+
 class ChatBot:
     def __init__(self, user_prompt):
         self.user_prompt = user_prompt
 
     def get_data(self):
-        pass
+        split_message = re.split(r'\s+|[,;?!.-]\s*', self.user_prompt.lower())
+        score_list = []
+
+        for content in contents:
+            response_score = 0
+            required_score = 0
+            required_words = content['required_words']
+
+            if required_words:
+                for word in split_message:
+                    if word in required_words:
+                        required_score += 1
+
+            if required_score == len(required_words):
+                for word in split_message:
+                    if word in content['user_input']:
+                        response_score += 1
+
+            score_list.append(response_score)
 
 
 if __name__ == "__main__":
-    chat_bot = ChatBot()
+    while True:
+        user_prompt = input("Enter a message: ")
+        chat_bot = ChatBot(user_prompt)
+        response = chat_bot.get_data()
+        print(response)
+
 
